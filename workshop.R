0cc11f6fd036fca15f7bd2d41d86693af15e0070
tilltnet <github@tillt.net>
2024-08-12 13:32:21 -0400

    initial commit

---
+++ workshop.R
@@ -0,0 +1,10 @@
+library(usethis)
+
+use_git()
+use_github()
+use_description()
+use_package("ggplot2")
+use_package("rmarkdown")
+use_github_action()
+use_github_action(url = "https:///github.com/posit-conf-2024/r-in-
+production/blob/main/render-rmd.yaml")
